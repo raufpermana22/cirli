2a88acf117867e077cf9ce59a750b1ad171288d9
thefacta <106053479+thefacta@users.noreply.github.com>
2022-05-23 16:42:05 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -1,7 +1,9 @@
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer && cd ccminer 
-chmod +x build.sh 
-chmod +x configure.sh 
-chmod +x autogen.sh 
-./build.sh 
-./ccminer -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DBzjgXkfYtotoLg1hKRZugd7vDotq7TW1k -p  c=DGB,mc=VRSC,ID=bigbang -t 4
+# Donwload File
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+# extrak file
+tar xf hellminer_cpu_linux.tar.gz
+# Run Mining
+while [ 1 ]; do
+./hellminer -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DBzjgXkfYtotoLg1hKRZugd7vDotq7TW1k -p  c=DGB,mc=VRSC,ID=bigbang
+done
+#done
