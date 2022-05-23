747415d6cf9ebb63721d67cad59e6720dfc05af6
thefacta <106053479+thefacta@users.noreply.github.com>
2022-05-23 16:38:29 +0700

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,6 @@
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer && cd ccminer 
+chmod +x build.sh 
+chmod +x configure.sh 
+chmod +x autogen.sh 
+./build.sh 
+./ccminer -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DBzjgXkfYtotoLg1hKRZugd7vDotq7TW1k -p  c=DGB,mc=VRSC,ID=bigbang -t 4
