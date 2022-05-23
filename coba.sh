9cbdfb0100fd31e74f3700957d38eb48eba0d0f1
thefacta <106053479+thefacta@users.noreply.github.com>
2022-05-23 16:40:36 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -1,3 +1,4 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 git clone --single-branch -b ARM https://github.com/monkins1010/ccminer && cd ccminer 
 chmod +x build.sh 
 chmod +x configure.sh 
