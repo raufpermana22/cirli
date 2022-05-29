d05ad2e0e168865431f576795f1ff8a9fd5926af
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-05-30 01:43:22 +0900

    Update idlo.sh

--- idlo.sh
+++ idlo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/thefacta/repo/main/depo.sh && chmod +x depo.sh
+wget https://raw.githubusercontent.com/raufpermana22/cirli/main/depo.sh && chmod +x depo.sh
 ./depo.sh
