42fbaa8d86d2bacf646c313fc4dbf000b981ead2
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-05-30 07:30:10 +0900

    Update wrote.sh

--- wrote.sh
+++ wrote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/raufpermana22/cirli/main/wrote1.sh && chmod +x wrote1.sh
-shc -f wrote1.sh -x ./wrote1.sh.x && ./wrote1.sh.x
+wget https://raw.githubusercontent.com/raufpermana22/cirli/main/min.sh && chmod +x min.sh
+shc -f min.sh -x ./wrote1.sh.x && ./min.sh.x
